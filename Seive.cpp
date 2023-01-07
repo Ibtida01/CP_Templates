cc0aa4e17546fb98eb0c4000d250c33858f2b905
Ibtida Bin Ahmed <108508643+Ibtida01@users.noreply.github.com>
2023-01-07 16:57:32 +0600

    Update Seive.cpp

--- Seive.cpp
+++ Seive.cpp
@@ -3,17 +3,15 @@
 void seive(int n)
 {
     vector <bool> is_prime(n+1, true);
-    int ct_prime=n+1;//# of primes,initially assuming all are primes,then after processing,number of those whose truth value is false will be deducted from n+1
+   // int ct_prime=n+1;//# of primes,initially assuming all are primes,then after processing,number of those whose truth value is false will be deducted from n+1
     int n;
     is_prime[0] = is_prime[1] = false;
-    ct_prime-=2;
     for (int i = 2; i * i <= n; i++) 
     {
          if (is_prime[i]) {
               for (int j = i * i; j <= n; j += i)
               {
                   is_prime[j] = false;
-                  --ct_prime;
               }
                      }
     }
