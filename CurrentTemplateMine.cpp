fd8c5608fb9e7a6e3cdc8881ab05816c1a66e715
Ibtida Bin Ahmed <108508643+Ibtida01@users.noreply.github.com>
2023-10-25 15:41:19 +0600

    Create CurrentTemplateMine.cpp

---
+++ CurrentTemplateMine.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+#include<iomanip>
+#include<cstring>
+#define dbg(x) cout << #x << " = " << x << endl;
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define pans cout << ans << "\n";
+#define endl "\n"
+#define pb push_back
+#define mp make_pair
+#define MAX 1000000000
+#define pii pair<int,int>
+#define EPS 1e-9
+// const ll mod=1e9+7;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+using namespace std;
+bool isEqualD(double x,double y)
+{
+    return (abs(x-y)<EPS) ? true : false;
+}
+void solve()
+{
+  
+}
+int main()
+{
+    //freopen("input.txt", "r", stdin);
+    //freopen("out.txt", "w", stdout);
+    clock_t tm;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //tm = clock();
+        solve();
+        //tm = clock() - tm;
+        //double time_taken = ((double)tm)/CLOCKS_PER_SEC;
+        //cout<<"time taken "<<time_taken<<endl;
+    }
+}
+  
