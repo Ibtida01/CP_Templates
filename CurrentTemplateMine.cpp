2f35da64f3dc9493738a8a7f3ffab717d87d3f1f
Ibtida Bin Ahmed <108508643+Ibtida01@users.noreply.github.com>
2024-01-01 18:52:46 +0600

    Update CurrentTemplateMine.cpp

--- CurrentTemplateMine.cpp
+++ CurrentTemplateMine.cpp
@@ -1,11 +1,13 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
 #include <bits/stdc++.h>
-#include <iostream>
-#include <cmath>
-#include<iomanip>
-#include<cstring>
 #define dbg(x) cout << #x << " = " << x << endl;
 #define fast_io ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
 #define pans cout << ans << "\n";
+#define trav(a, x) for(auto& a : x)
+#define FOR(i, a, b) for (int i=(a); i<=(signed)(b); i++)
+#define ROF(i, a, b) for (int i=(a); i>=(signed)(b); i--)
+#define F0R(i, a) for (int i=0; i<(signed)(a); i++)
 #define endl "\n"
 #define pb push_back
 #define mp make_pair
