a177e994a4d6e2e48f9362f2704744a991aab7f2
Ibtida Bin Ahmed <108508643+Ibtida01@users.noreply.github.com>
2024-01-10 13:28:54 +0600

    Update CountingSort_linear_sorting.cpp

--- CountingSort_linear_sorting.cpp
+++ CountingSort_linear_sorting.cpp
@@ -1,13 +1,10 @@
-void countingSort(vector<int>& arr, int startingIdx, int endingIdx, int minimum_val, int maximum_val)//arr = array to be sorted, might of edges too (for kruskal's algo)
+template <typename t>
+void countingSort(vector<t>& arr, int startingIdx, int endingIdx, int minimum_val, int maximum_val)//arr = array to be sorted, might of edges too (for kruskal's algo)
 {
     int arr_size=endingIdx-startingIdx+1;
     int i,k=0,sizeTemp = maximum_val-minimum_val+1;
-    int temp[sizeTemp];
-
-    // Initializing all element of counting array with 0
-    for(i=0; i<sizeTemp; i++){
-        temp[i] = 0;
-    }
+    vector<int> temp(sizeTemp,0);
+    // Initialized all element of counting array with 0
 
     //Store the frequency of each element of the original array
     //at the relative position in counting array
