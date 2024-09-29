a855b3481b6dfdf8f2d078306f4923fbb4333dc6
Ibtida Bin Ahmed <108508643+Ibtida01@users.noreply.github.com>
2024-09-29 08:37:30 +0600

    Create InteractiveCP.cpp

---
+++ InteractiveCP.cpp
@@ -0,0 +1,51 @@
+//This code is for the link's problem:
+//https://codeforces.com/contest/2013/problem/C
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <array>
+ 
+using namespace std;
+ 
+bool ask(string t) {
+    cout << "? " << t << endl;
+    int res;
+    cin >> res;
+    return res;
+}
+ 
+void result(string s) {
+    cout << "! " << s << endl;
+}
+ 
+void solve() {
+    int n;
+    cin >> n;
+    string cur;
+    while (cur.size() < n) {
+        if (ask(cur + "0")) {
+            cur += "0";
+        } else if (ask(cur + "1")) {
+            cur += "1";
+        } else {
+            break;
+        }
+    }
+    while ((int) cur.size() < n) {
+        if (ask("0" + cur)) {
+            cur = "0" + cur;
+        } else{
+            cur = "1" + cur;
+        }
+    }
+    result(cur);
+}
+ 
+int main() {
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+}
