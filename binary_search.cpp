4794540312c43df46e953219ef204d4b2a3f298b
Ibtida Bin Ahmed <108508643+Ibtida01@users.noreply.github.com>
2025-02-10 10:49:48 +0600

    Update binary_search.cpp

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,11 +1,11 @@
-l=0, r=n-1
-while( l<=r)
-{
-  mid = l + (r-l)/2;
-  if(a[mid]==target)
-    return mid;
-  if(a[mid]<target)
-    l=mid+1;
-  else r=mid-1;
-  return -1;
-}
+    l=0, r=n-1
+    while( l<=r)
+    {
+      mid = l + (r-l)/2;
+      if(a[mid]==target)
+        return mid;
+      if(a[mid]<target)
+        l=mid+1;
+      else r=mid-1;
+      return -1;
+    }
